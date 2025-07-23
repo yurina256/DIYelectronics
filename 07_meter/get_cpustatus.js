27cb945bd72e094ea11c8096da88c6e93c7e3581
yurina256 <info@yurina256.net>
2025-07-23 09:21:48 +0900

    exports status

--- 07_meter/get_cpustatus.js
+++ 07_meter/get_cpustatus.js
@@ -1,40 +1,47 @@
 const http = require('http');
+const { resolve } = require('path');
 
-const options = {
-  hostname: 'localhost',
-  port: 8085,
-  path: `/data.json`,
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-};
 
-const req = http.request(options, res => {
+exports.get_stats = function get_stats() {
+  const options = {
+    hostname: 'localhost',
+    port: 8085,
+    path: `/data.json`,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const req = http.request(options, res => {
     let data = '';
 
     // データがチャンクで受信されるたびに実行
     res.on('data', (chunk) => {
-        data += chunk;
+      data += chunk;
     });
 
     // 全てのデータが受信された後に実行
     res.on('end', () => {
-        try {
-            const jsonData = JSON.parse(data);
-            console.log('受信したJSONデータ:', jsonData);
-            console.log(`cputemp: ${jsonData.Children[0].Children[1].Children[1].Children[0].Value}`);
-            console.log(`cpuusage: ${jsonData.Children[0].Children[1].Children[2].Children[0].Value}`);
-            console.log(`memoryusage: ${jsonData.Children[0].Children[2].Children[0].Children[0].Value}`);
-        } catch (error) {
-            console.error('JSONのパースに失敗しました:', error);
-            console.error('受信した生データ:', data);
-        }
+      try {
+        const jsonData = JSON.parse(data);
+        console.log('受信したJSONデータ:', jsonData);
+        console.log(`cputemp: ${jsonData.Children[0].Children[1].Children[1].Children[0].Value}`);
+        console.log(`cpuusage: ${jsonData.Children[0].Children[1].Children[2].Children[0].Value}`);
+        console.log(`memoryusage: ${jsonData.Children[0].Children[2].Children[0].Children[0].Value}`);
+        const cpuusage = jsonData.Children[0].Children[1].Children[2].Children[0].Value;
+        const memoryusage = jsonData.Children[0].Children[2].Children[0].Children[0].Value;
+        resolve(cpuusage * 1000 + memoryusage);
+      } catch (error) {
+        console.error('JSONのパースに失敗しました:', error);
+        console.error('受信した生データ:', data);
+      }
     });
-});
+  });
 
-req.on('error', error => {
-  console.error(error);
-});
+  req.on('error', error => {
+    console.error(error);
+  });
 
-req.end();
+  req.end();
+}
